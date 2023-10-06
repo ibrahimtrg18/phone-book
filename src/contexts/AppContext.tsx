e6d86abf45af33b3d1b6141c2b5ade23892bf837
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-10-06 23:18:36 +0700

    chore: split code to own perpose

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext } from "react";
+import { useApp } from "@/hooks/useApp";
 
 export const AppContext = createContext<null | ReturnType<typeof useApp>>(null);
 
@@ -21,89 +22,4 @@ export const AppContextProvider = ({
   const value = useApp();
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
-};
-
-type AppState = {
-  title: string;
-  showGoBack?: boolean;
-  actionButton?: {
-    show?: boolean;
-    icon?: React.ReactNode;
-    link?: object;
-  };
-};
-
-type AppAction = {
-  type: string;
-  payload?: any;
-};
-
-const initialAppState: AppState = {
-  title: "",
-  showGoBack: false,
-  actionButton: {
-    show: false,
-    icon: null,
-    link: {},
-  },
-};
-
-const APPBAR_ACTION = {
-  SET_TITLE: "SET_TITLE",
-  SET_SHOW_GO_BACK: "SET_SHOW_GO_BACK",
-  SET_SHOW_ACTION_BUTTON: "SET_SHOW_ACTION_BUTTON",
-  SET_APP_STATE: "SET_APP_STATE",
-};
-
-const appReducer = (state: AppState, action: AppAction) => {
-  switch (action.type) {
-    case APPBAR_ACTION.SET_TITLE:
-      return {
-        ...state,
-        title: action.payload,
-      };
-
-    case APPBAR_ACTION.SET_SHOW_GO_BACK:
-      return {
-        ...state,
-        showGoBack: action.payload,
-      };
-
-    case APPBAR_ACTION.SET_APP_STATE:
-      return {
-        ...state,
-        ...action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export const useApp = () => {
-  const [{ title, showGoBack, actionButton }, dispatch] = useReducer(
-    appReducer,
-    initialAppState
-  );
-
-  const setTitleAppbar = (payload: string) => {
-    dispatch({ type: APPBAR_ACTION.SET_TITLE, payload });
-  };
-
-  const setShowGoBack = (payload: boolean) => {
-    dispatch({ type: APPBAR_ACTION.SET_SHOW_GO_BACK, payload });
-  };
-
-  const setAppState = (payload: Partial<AppState>) => {
-    dispatch({ type: APPBAR_ACTION.SET_APP_STATE, payload });
-  };
-
-  return {
-    title,
-    showGoBack,
-    actionButton,
-    setTitleAppbar,
-    setShowGoBack,
-    setAppState,
-  };
 };
---
+++ src/hooks/useApp.tsx
@@ -0,0 +1,86 @@
+import { useReducer } from "react";
+
+type AppState = {
+  title: string;
+  showGoBack?: boolean;
+  actionButton?: {
+    show?: boolean;
+    icon?: React.ReactNode;
+    link?: object;
+  };
+};
+
+type AppAction = {
+  type: string;
+  payload?: any;
+};
+
+const initialAppState: AppState = {
+  title: "",
+  showGoBack: false,
+  actionButton: {
+    show: false,
+    icon: null,
+    link: {},
+  },
+};
+
+const APPBAR_ACTION = {
+  SET_TITLE: "SET_TITLE",
+  SET_SHOW_GO_BACK: "SET_SHOW_GO_BACK",
+  SET_SHOW_ACTION_BUTTON: "SET_SHOW_ACTION_BUTTON",
+  SET_APP_STATE: "SET_APP_STATE",
+};
+
+const appReducer = (state: AppState, action: AppAction) => {
+  switch (action.type) {
+    case APPBAR_ACTION.SET_TITLE:
+      return {
+        ...state,
+        title: action.payload,
+      };
+
+    case APPBAR_ACTION.SET_SHOW_GO_BACK:
+      return {
+        ...state,
+        showGoBack: action.payload,
+      };
+
+    case APPBAR_ACTION.SET_APP_STATE:
+      return {
+        ...state,
+        ...action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const useApp = () => {
+  const [{ title, showGoBack, actionButton }, dispatch] = useReducer(
+    appReducer,
+    initialAppState
+  );
+
+  const setTitleAppbar = (payload: string) => {
+    dispatch({ type: APPBAR_ACTION.SET_TITLE, payload });
+  };
+
+  const setShowGoBack = (payload: boolean) => {
+    dispatch({ type: APPBAR_ACTION.SET_SHOW_GO_BACK, payload });
+  };
+
+  const setAppState = (payload: Partial<AppState>) => {
+    dispatch({ type: APPBAR_ACTION.SET_APP_STATE, payload });
+  };
+
+  return {
+    title,
+    showGoBack,
+    actionButton,
+    setTitleAppbar,
+    setShowGoBack,
+    setAppState,
+  };
+};
